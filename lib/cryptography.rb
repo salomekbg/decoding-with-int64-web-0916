4514b2e2ac9d07285b513aafb116f17a4ea21a33
Katie Hoffman <ktahoffman@gmail.com>
2014-08-20 11:32:17 -0400

    merge branches in attempt to trigger build

--- lib/cryptography.rb
+++ lib/cryptography.rb
@@ -1,7 +1,27 @@
+def valid_chars
+  "acdegilmnoprstuw"
+end
+
 def encode_int64(message)
-  # code this method first
+  encoded_message = 7
+  i = 0
+  while i < message.length
+    if valid_chars.include?(message[i])
+      encoded_message = 37 * encoded_message + valid_chars.index(message[i])
+    else
+      return "The word should only contain letters in #{valid_chars}"
+    end
+    i += 1
+  end
+  encoded_message
 end
 
 def decode_int64(encoded_message)
-  # code this second
+  message = ""
+  until encoded_message == 7
+    i = encoded_message % 37
+    message << valid_chars[i]
+    encoded_message = (encoded_message - i ) / 37
+  end
+  message.reverse
 end
