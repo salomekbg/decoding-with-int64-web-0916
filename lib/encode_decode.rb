bbdb1b9f0ffe4f4533c3f354dbd5fe547575cad2
SluvsP <salomekbg@gmail.com>
2017-01-20 18:21:18 -0500

    Done.

--- lib/encode_decode.rb
+++ lib/encode_decode.rb
@@ -1,7 +1,23 @@
 def encode_int64(message)
-  # code this method first
+  h = 7
+  letters = "acdegilmnoprstuw"
+  message.split('').each do |letter|
+    if letters.include?(letter)
+      h = (h * 37 + letters.index(letter))
+    else
+      return "The word should only contain letters in acdegilmnoprstuw"
+    end
+  end
+  h
 end
 
 def decode_int64(encoded_message)
-  # code this second
+  letters = "acdegilmnoprstuw"
+  message = ""
+  while encoded_message != 7
+    remainder = encoded_message % 37
+    message = letters[remainder] + message
+    encoded_message = (encoded_message - remainder) / 37
+  end
+  message
 end
